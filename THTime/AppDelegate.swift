70ff382a57f1e89d7b5a2ebd08a40f2ff42a07fe
romankobosil <romankobosil@me.com>
2016-10-11 12:54:57 +0200

    Initial Commit

---
+++ THTime/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  THTime
+//
+//  Created by Roman Kobosil on 11.10.16.
+//  Copyright © 2016 Roman Kobosil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ THTime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  THTime
+//
+//  Created by Roman Kobosil on 11.10.16.
+//  Copyright © 2016 Roman Kobosil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
